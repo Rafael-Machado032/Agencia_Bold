671b1fefa6e6c5efb830c9017f30b5577adc8608
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2026-06-02 22:35:09 -0300

    teste

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\MensagemController;
 
 /*
 |--------------------------------------------------------------------------
-| Rotas Públicas (Site / Visitante) teste
+| Rotas Públicas (Site / Visitante)
 |--------------------------------------------------------------------------
 */
 
