0b246c05c3b3074385dfaaf396eba98e59269893
Rafael-Machado032 <api.rafaelmachado@gmail.com>
2026-06-06 00:22:45 -0300

    a

--- frontend/components/site/Incetivo.tsx
+++ frontend/components/site/Incetivo.tsx
@@ -6,7 +6,7 @@ export default function Incetivo() {
             <div className='text-white text-center flex flex-col items-center gap-2'>
                 <h2 className='text-4xl'>Vamos Começar Seu Projeto?</h2>
                 <p>Fale agora mesmo com um de nossos consultores!</p>
-                <button className=" bg-yellow-300 text-blue-800 px-4 py-2 rounded-lg hover:bg-yellow-600 transition">Entra em Contato</button>
+                <a href="#contato" className=" bg-yellow-300 text-blue-800 px-4 py-2 rounded-lg hover:bg-yellow-600 transition">Entrar em Contato</a>
             </div>
         </section>
     )
